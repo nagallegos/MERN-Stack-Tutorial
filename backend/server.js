8f0554260ea8f7091b2221c00872bff753867d21
nagallegos <113269909+nagallegos@users.noreply.github.com>
2024-01-31 22:57:20 -0600

    Added routes folder and filled with routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
+
+// middleware
+app.use(express.json())
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
@@ -11,9 +15,7 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.get('/', (req, res) => {
-  res.json({msg: 'Welcome to the app'})
-})
+app.use('/api/workouts/', workoutRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => {
